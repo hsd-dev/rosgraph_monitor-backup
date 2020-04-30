df36460db32dd769c46c797fdcc839b0c0dcd653
Darko <darko.bozhinoski@gmail.com>
2020-04-30 15:29:32 +0200

    EnergyObserver

--- src/rosgraph_monitor/observers/quality_observer.py
+++ src/rosgraph_monitor/observers/quality_observer.py
@@ -1,11 +1,12 @@
 from rosgraph_monitor.observer import TopicObserver
 from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
 
 
 class QualityObserver(TopicObserver):
     def __init__(self, name):
-        topics = [("/speed", Int32), ("/accel", Int32)]     # list of pairs
+        topics = [("/power_load", Float32)]     # list of pairs
 
         super(QualityObserver, self).__init__(
             name, 10, topics)
@@ -13,14 +14,14 @@ class QualityObserver(TopicObserver):
     def calculate_attr(self, msgs):
         status_msg = DiagnosticStatus()
 
-        attr = msgs[0].data + msgs[1].data
-        print("{0} + {1}".format(msgs[0].data, msgs[1].data))
+        attr = msgs[0].data
+        print("{0}".format(msgs[0].data))
 
         status_msg = DiagnosticStatus()
         status_msg.level = DiagnosticStatus.OK
         status_msg.name = self._id
         status_msg.values.append(
-            KeyValue("enery", str(attr)))
+            KeyValue("energy", str(attr)))
         status_msg.message = "QA status"
 
         return status_msg
